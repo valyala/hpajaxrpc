9cce9d429081e21bc7c7462d0f941990ee18dd2b
Aliaksandr Valialkin <valyala@gmail.com>
2011-10-20 19:42:09 +0300

    split lines longer than 80 chars

--- hpajaxrpc.js
+++ hpajaxrpc.js
@@ -44,7 +44,8 @@ hpajaxrpc = (function() {
   };
 
   JsonRpc.prototype = {
-    run: function(rpc_endpoint, request_data, response_callback, finalize_callback) {
+    run: function(rpc_endpoint, request_data, response_callback,
+        finalize_callback) {
       var request_text;
       try {
         request_text = JSON.stringify(request_data);
@@ -75,7 +76,8 @@ hpajaxrpc = (function() {
             response_callback(response_data);
           }
           catch(e) {
-            issueCallback(finalize_callback, statusCodes.RESPONSE_CALLBACK_ERROR, e);
+            issueCallback(finalize_callback,
+                statusCodes.RESPONSE_CALLBACK_ERROR, e);
             return;
           }
         }
@@ -93,7 +95,8 @@ hpajaxrpc = (function() {
   };
 
   QueuedRpc.prototype = {
-    _startRpc: function(rpc_endpoint, request_data, response_callback, finalize_callback) {
+    _startRpc: function(rpc_endpoint, request_data, response_callback,
+        finalize_callback) {
       var that = this;
       var queued_finalize_callback = function(status_code, status_data) {
         if (that._queued_rpc_args.length > 0) {
@@ -104,7 +107,8 @@ hpajaxrpc = (function() {
         }
         issueCallback(finalize_callback, status_code, status_data);
       };
-      this._base_rpc_call(rpc_endpoint, request_data, response_callback, queued_finalize_callback);
+      this._base_rpc_call(rpc_endpoint, request_data, response_callback,
+          queued_finalize_callback);
     },
 
     _issueNextRpc: function() {
@@ -112,8 +116,10 @@ hpajaxrpc = (function() {
       this._startRpc.apply(this, rpc_args);
     },
 
-    run: function(rpc_endpoint, request_data, response_callback, finalize_callback) {
-      this._queued_rpc_args.push([rpc_endpoint, request_data, response_callback, finalize_callback]);
+    run: function(rpc_endpoint, request_data, response_callback,
+        finalize_callback) {
+      this._queued_rpc_args.push([rpc_endpoint, request_data, response_callback,
+          finalize_callback]);
       if (!this._is_rpc_in_flight) {
         this._is_rpc_in_flight = true;
         this._issueNextRpc();
@@ -142,10 +148,14 @@ hpajaxrpc = (function() {
       this._batched_rpc_args = [];
       var batched_response_callback = function(batched_response_data) {
         if (!(batched_response_data instanceof Array)) {
-          throw 'batched_response_data='+JSON.stringify(batched_response_data)+' should be instance of Array';
+          throw 'batched_response_data=' +
+              JSON.stringify(batched_response_data) +
+              ' should be an instance of Array';
         }
         if (batched_response_data.length != response_callbacks.length) {
-          throw 'unexpected batched_response_data size='+batched_response_data.length+', expected='+response_callbacks.length;
+          throw 'unexpected batched_response_data size=' +
+              batched_response_data.length + ', expected size=' +
+              response_callbacks.length;
         }
         batched_response_data.forEach(function(response_data, index) {
           issueCallback(response_callbacks[index], response_data);
@@ -163,7 +173,8 @@ hpajaxrpc = (function() {
           issueCallback(finalize_callback, status_code, status_data);
         });
       };
-      this._base_rpc_call(batched_request_data, batched_response_callback, batched_finalize_callback);
+      this._base_rpc_call(batched_request_data, batched_response_callback,
+          batched_finalize_callback);
     },
 
     _issueRpcDelayed: function() {
@@ -180,7 +191,8 @@ hpajaxrpc = (function() {
       if (batched_rpc_args.length == 0) {
         this._first_rpc_time = Date.now();
       }
-      batched_rpc_args.push([request_data, response_callback, finalize_callback]);
+      batched_rpc_args.push(
+          [request_data, response_callback, finalize_callback]);
       if (!this._is_rpc_in_flight) {
         this._is_rpc_in_flight = true;
         this._issueRpcDelayed();
@@ -221,7 +233,8 @@ hpajaxrpc = (function() {
         }
         issueCallback(finalize_callback, status_code, status_data);
       };
-      this._base_rpc_call(request_data, response_callback, rate_limited_finalize_callback);
+      this._base_rpc_call(request_data, response_callback,
+          rate_limited_finalize_callback);
     },
 
     run: function(request_data, response_callback, finalize_callback) {
@@ -231,7 +244,8 @@ hpajaxrpc = (function() {
       else {
         issueCallback(this._last_rpc_args[2], statusCodes.SUCCESS);
       }
-      this._last_rpc_args = [request_data, response_callback, finalize_callback];
+      this._last_rpc_args = [request_data, response_callback,
+          finalize_callback];
       if (!this._is_rpc_in_flight) {
         this._is_rpc_in_flight = true;
         this._issueRpc();
